dbf32f6bfc7c2351583a9051d9af63943fa89921
jakub <ookubaoo2@gmail.com>
2022-11-27 12:22:11 +0100

    Structure impementation

---
+++ Meet2gether/src/main/java/org/infoshareacademy/javaholics/App.java
@@ -0,0 +1,13 @@
+package org.infoshareacademy.javaholics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
