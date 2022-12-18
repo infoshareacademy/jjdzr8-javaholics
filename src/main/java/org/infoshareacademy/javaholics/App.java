307fe6fd5cc6dd9456e8a4887d62fad11be45651
Kamil Koltan <kam.koltan@gmail.com>
2022-12-18 12:20:08 +0100

    Adding Menu to menu class and changing try/catch exception.

--- src/main/java/org/infoshareacademy/javaholics/App.java
+++ src/main/java/org/infoshareacademy/javaholics/App.java
@@ -7,17 +7,9 @@ package org.infoshareacademy.javaholics;
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello Javaholics");
+        System.out.println("Hello Javaholics");
 
         Menu menu = new Menu();
-
-        System.out.println("1. Dodaj nową trasę");
-        System.out.println("2. Wyszukaj trasę");
-        System.out.println("3. Edytuj trasę");
-        System.out.println("4. Usuń trasę");
-        System.out.println("5. Dodaj Event");
-        System.out.println("6. Wyszukaj Event");
-
         menu.printMenu();
     }
 }
--- src/main/java/org/infoshareacademy/javaholics/Menu.java
+++ src/main/java/org/infoshareacademy/javaholics/Menu.java
@@ -1,38 +1,52 @@
 package org.infoshareacademy.javaholics;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
 
     public void printMenu() {
+        System.out.println("1. Dodaj nową trasę");
+        System.out.println("2. Wyszukaj trasę");
+        System.out.println("3. Edytuj trasę");
+        System.out.println("4. Usuń trasę");
+        System.out.println("5. Dodaj Event");
+        System.out.println("6. Wyszukaj Event");
+
         boolean isWrongAnswer;
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Co chcesz zrobić: ");
 
         do {
             isWrongAnswer = false;
-            System.out.print("Co chcesz zrobić: ");
-            switch (scanner.nextInt()) {
-                case 1:
-                    System.out.println("Edycja");
-                    break;
-                case 2:
-                    System.out.println("Dodanie trasy");
-                    break;
-                case 3:
-                    System.out.println("Zapisanie trasy");
-                    break;
-                case 4:
-                    System.out.println("Usuń trasę");
-                    break;
-                case 5:
-                    System.out.println("Dodaj Event");
-                    break;
-                case 6:
-                    System.out.println("Wyszukaj Event");
-                    break;
-                default:
-                    System.out.println("Wybierz opcję od 1 do 6");
-                    isWrongAnswer = true;
+            try {
+                switch (scanner.nextInt()) {
+                    case 1:
+                        System.out.println("Edycja");
+                        break;
+                    case 2:
+                        System.out.println("Dodanie trasy");
+                        break;
+                    case 3:
+                        System.out.println("Zapisanie trasy");
+                        break;
+                    case 4:
+                        System.out.println("Usuń trasę");
+                        break;
+                    case 5:
+                        System.out.println("Dodaj Event");
+                        break;
+                    case 6:
+                        System.out.println("Wyszukaj Event");
+                        break;
+                    default:
+                        System.out.println("Wybierz opcję od 1 do 6");
+                        isWrongAnswer = true;
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Wybierz proszę widoczne opcje powyżej.");
+                break;
             }
         } while (isWrongAnswer);
     }
