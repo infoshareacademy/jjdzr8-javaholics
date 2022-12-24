ce2c7e0c2bdf7367f3490d533fbcaaf3442a047d
Michał Gołombiewski <m.golombiewski@gmail.com>
2022-12-24 12:16:44 +0100

    Reading and writting current IDs' numeber of User, Route, Eventinto JSON file.

--- src/main/java/org/infoshareacademy/javaholics/App.java
+++ src/main/java/org/infoshareacademy/javaholics/App.java
@@ -1,13 +1,19 @@
 package org.infoshareacademy.javaholics;
 
+
+import org.infoshareacademy.javaholics.utils.FileUtils;
+import org.infoshareacademy.javaholics.utils.IpsNumbers;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Javaholics");
+public class App {
+    public static void main(String[] args) {
+        IpsNumbers ipsNumbers = FileUtils.readUsersJsonFile();
+        ipsNumbers.setIpUser(2);
+        ipsNumbers.setIpRoute(2);
+        ipsNumbers.setIpEvent(3);
+        FileUtils.saveUsersToJsonFile(ipsNumbers);
     }
 }
