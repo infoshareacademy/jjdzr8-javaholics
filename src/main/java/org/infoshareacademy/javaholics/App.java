6fa321b7c98245c3d39dd7950a2fc384f804316a
jakub <ookubaoo2@gmail.com>
2022-12-30 14:24:28 +0100

    menu z poprawkami

--- src/main/java/org/infoshareacademy/javaholics/App.java
+++ src/main/java/org/infoshareacademy/javaholics/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Javaholics");
+        Menu menu = new Menu();
+        menu.printMenu();
     }
 }
