858ae29ed7c603f2a09fdaaf960fc3f840461a67
jakub <ookubaoo2@gmail.com>
2023-01-21 22:28:13 +0100

    Działa

--- src/main/java/org/infoshareacademy/javaholics/App.java
+++ src/main/java/org/infoshareacademy/javaholics/App.java
@@ -1,13 +1,6 @@
 package org.infoshareacademy.javaholics;
 
 
-import org.infoshareacademy.javaholics.event.EventService;
-import org.infoshareacademy.javaholics.event.Events;
-//import org.infoshareacademy.javaholics.route.RouteSearch;
-import org.infoshareacademy.javaholics.route.RouteService;
-import org.infoshareacademy.javaholics.user.UserService;
-import org.infoshareacademy.javaholics.utils.FileService;
-
 public class App {
     public static void main(String[] args) {
         Menu menu = new Menu();
